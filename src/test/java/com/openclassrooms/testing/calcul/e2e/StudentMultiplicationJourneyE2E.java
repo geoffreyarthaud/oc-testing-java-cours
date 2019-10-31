f32e15d0a8f53d2db27a1cd539fff567b36069bc
Geoffrey ARTHAUD <geoffrey.arthaud@developpement-durable.gouv.fr>
2019-10-31 13:15:03 +0100

    Added Page Object

--- src/test/java/com/openclassrooms/testing/calcul/e2e/StudentMultiplicationJourneyE2E.java
+++ src/test/java/com/openclassrooms/testing/calcul/e2e/StudentMultiplicationJourneyE2E.java
@@ -7,15 +7,13 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.openclassrooms.testing.calcul.e2e.page.CalculatorPage;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -48,25 +46,28 @@ public class StudentMultiplicationJourneyE2E {
 
 	@Test
 	public void aStudentUsesTheCalculatorToMultiplyTwoBySixteen() {
-
 		// GIVEN
 		webDriver.get(baseUrl);
-		final WebElement leftField = webDriver.findElement(By.id("left"));
-		final WebElement typeDropdown = webDriver.findElement(By.id("type"));
-		final WebElement rightField = webDriver.findElement(By.id("right"));
-		final WebElement submitButton = webDriver.findElement(By.id("submit"));
+		final CalculatorPage calculatorPage = new CalculatorPage(webDriver);
 
 		// WHEN
-		leftField.sendKeys("2");
-		typeDropdown.sendKeys("x");
-		rightField.sendKeys("16");
-		submitButton.click();
+		final String solution = calculatorPage.multiply("2", "16");
 
 		// THEN
-		final WebDriverWait waiter = new WebDriverWait(webDriver, 5);
-		final WebElement solutionElement = waiter.until(
-				ExpectedConditions.presenceOfElementLocated(By.id("solution")));
-		final String solution = solutionElement.getText();
 		assertThat(solution).isEqualTo("32"); // 2 x 16
+
+	}
+
+	@Test
+	public void aStudentUsesTheCalculatorToAddTwoToSixteen() throws InterruptedException {
+		// GIVEN
+		webDriver.get(baseUrl);
+		final CalculatorPage calculatorPage = new CalculatorPage(webDriver);
+
+		// WHEN
+		final String solution = calculatorPage.add("2", "16");
+
+		// THEN
+		assertThat(solution).isEqualTo("18"); // 2 + 16
 	}
 }
